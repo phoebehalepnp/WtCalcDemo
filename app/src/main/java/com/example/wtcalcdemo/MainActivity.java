292d85b0e7c691825288bfa1d15bdcbcf50334f2
Le <let59@student.douglascollege.ca>
2024-02-08 10:28:44 -0800

    Second changes done from local

--- app/src/main/java/com/example/wtcalcdemo/MainActivity.java
+++ app/src/main/java/com/example/wtcalcdemo/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     TextView txtViewResults;
 
+    //Added another line from local on Feb 8
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
